3c109bc3416b954bec855f60d1e13dcb204070f0
turinglabs <sebastiano.cataudo@gmail.com>
2024-12-09 15:39:57 +0100

    adding new swap tokens

--- test/arbitrum/03-get-tokens-by-buildingblock.ts
+++ test/arbitrum/03-get-tokens-by-buildingblock.ts
@@ -5,6 +5,7 @@ import { BuildingBlock } from '@factordao/sdk-studio';
 async function main() {
   const tokens = new FactorTokenlist(ChainId.ARBITRUM_ONE);
   console.table(tokens.getTokensByBuildingBlock(BuildingBlock.LEND));
+  console.log(tokens.protocols);
 }
 
 main();
