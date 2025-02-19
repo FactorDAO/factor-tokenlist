95f36aa32e6356b6049f22f046e278f3162f9fe5
turinglabs <sebastiano.cataudo@gmail.com>
2025-02-19 20:16:59 +0100

    protocol example

--- test/arbitrum/02-get-tokens-by-protocol.ts
+++ test/arbitrum/02-get-tokens-by-protocol.ts
@@ -2,7 +2,10 @@ import { ChainId, FactorTokenlist, Protocols } from '../../src';
 
 async function main() {
   const tokens = new FactorTokenlist(ChainId.ARBITRUM_ONE);
-  console.table(tokens.getTokensByProtocol(Protocols.SILO));
+  for (const protocol of Object.values(Protocols)) {
+    console.log(protocol);
+    console.table(tokens.getTokensByProtocol(protocol));
+  }
 }
 
 main();
